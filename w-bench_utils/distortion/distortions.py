3625c4d1e5a360ff0c39737a3614969e07802b5b
Shilin-LU <shilin002@e.ntu.edu.sg>
2025-03-06 11:39:20 +0800

    update

--- w-bench_utils/distortion/distortions.py
+++ w-bench_utils/distortion/distortions.py
@@ -1,8 +1,8 @@
-import random
 from PIL import Image, ImageFilter, ImageEnhance
 import numpy as np
-import torch, io, os
-from distortion_utils import set_random_seed, to_tensor, to_pil
+import torch, io, os, argparse, random
+from utils import set_random_seed, to_tensor, to_pil
+from tqdm import tqdm
 
 
 distortion_strength_paras = dict(
@@ -77,7 +77,7 @@ def apply_single_distortion(image, distortion_type, strength=None, distortion_se
             <= max(*distortion_strength_paras[distortion_type])
         )
 
-    elif distortion_type == "brightness":
+    if distortion_type == "brightness":
         factor = (
             strength
             if strength is not None
@@ -130,50 +130,32 @@ def apply_single_distortion(image, distortion_type, strength=None, distortion_se
     return distorted_image
 
 if __name__ == '__main__':
-    models = [
-    # '01_MBRS', 
-    # '02_CIN', 
-    # '03_PIMoG', 
-    # '04_RivaGAN', 
-    # '05_SepMark', 
-    # '07_RoSteALS', 
-    # '08_DWTDCT', 
-    # '09_DWTDCTSVD', 
-    # '10_HIDDEN', 
-    # '12_SSL', 
-    # '14_RAW', 
-    # '15_StegaStamp', 
-    # '17_HIDDEN_CROP', 
-    # '18_TrustMark_C',
-    # '20_EditGuard', 
-    # '21_VINE_R', 
-    # '22_VINE_B'
-    ]
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--wm_images_folder', type=str, default="./vine_encoded_wbench")
+    parser.add_argument('--edited_output_folder', type=str, default="./output/distorted_wmed_wbench")
+    args = parser.parse_args() 
+    
     steps = {
-        'resizedcrop': (0.5, 0.7, 0.9, 1.1, 1.3),
-        'erasing': (0.05, 0.1, 0.15, 0.2, 0.25),
         'brightness': (1.2, 1.4, 1.6, 1.8, 2.0),
         'contrast': (1.2, 1.4, 1.6, 1.8, 2.0),
         'blurring': (1, 3, 5, 7, 9),
         'noise': (0.02, 0.04, 0.06, 0.08, 0.1),
         'compression': (10, 25, 40, 55, 70)
     }
-    root = '/ntuzfs/shilin/Shilin/watermark/'
-    for m in models:
-        for c in ['INVERSION']:
-            for i in os.listdir(os.path.join(root, m, '512', c)):
-                path = os.path.join(root, m, '512', c, i)
-                image = Image.open(path)
-                #for distortion_type in distortion_strength_paras:
-                for distortion_type in ['resizedcrop']:
-                    for step in steps[distortion_type]:
-                        save_path = os.path.join('/ntuzfs/shilin/Shilin/watermark/edited_result/distortion', m, '512', c, distortion_type, str(step))
-                        os.makedirs(save_path, exist_ok=True)
-                        image_edited = apply_single_distortion(image, distortion_type, strength=step)
-                        image_edited = to_pil(to_tensor([image_edited], norm_type=None).clamp(0, 1), norm_type=None)[0]
-                        assert np.min(np.array(image_edited)) >= 0 and np.max(np.array(image_edited)) <= 255
-                        assert image_edited.size == (512, 512)
-                        save_path = os.path.join(save_path, i)
-                        print(save_path)
-                        image_edited.save(save_path)
+
+    for c in ['DISTORTION_1K']:
+        for i in tqdm(os.listdir(os.path.join(args.wm_images_folder, '512', c))):
+            path = os.path.join(args.wm_images_folder, '512', c, i)
+            image = Image.open(path)
+            #for distortion_type in distortion_strength_paras:
+            for distortion_type in ['brightness', 'contrast', 'blurring', 'noise', 'compression']:
+                for step in steps[distortion_type]:
+                    save_path = os.path.join(args.edited_output_folder, '512', c, distortion_type, str(step))
+                    os.makedirs(save_path, exist_ok=True)
+                    image_edited = apply_single_distortion(image, distortion_type, strength=step)
+                    image_edited = to_pil(to_tensor([image_edited], norm_type=None).clamp(0, 1), norm_type=None)[0]
+                    assert np.min(np.array(image_edited)) >= 0 and np.max(np.array(image_edited)) <= 255
+                    assert image_edited.size == (512, 512)
+                    save_path = os.path.join(save_path, i)
+                    print(save_path)
+                    image_edited.save(save_path)
